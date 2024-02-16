8b054d70db6bfdd1b7adcc5c130ce3781e80a8b9
Antikainen Valtteri Aleks <valtteri.a.antikainen@student.jyu.fi>
2024-02-16 14:57:09 +0200

    tyo3 valmis

--- src/src/fxrubkinkuutio/RubikinkuutioAlkuController.java
+++ src/src/fxrubkinkuutio/RubikinkuutioAlkuController.java
@@ -1,13 +1,45 @@
 package fxrubkinkuutio;
 
+import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import fi.jyu.mit.fxgui.*;
+import javafx.application.Application;
 
-/*
- * 
+/**
+ * @author Valtteri
+ * @version 16.2.2024
+ *
  */
-public class RubikinkuutioAlkuController {
+public class RubikinkuutioAlkuController extends Application {
     
-    
-    private static void launch(String[] args) {
-        // TODO Auto-generated method stub
+    @FXML private void handleAloita() {
+        Stage stage = new Stage();
+        start(stage);
     }
+    
+    @FXML private void handleLopeta() {
+        Platform.exit();
+    }
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("RubikinkuutioGUIView.fxml"));
+            Scene scene = new Scene(root,800,480);
+            scene.getStylesheets().add(getClass().getResource("rubikinkuutio.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
--- src/src/fxrubkinkuutio/RubikinkuutioGUIController.java
+++ src/src/fxrubkinkuutio/RubikinkuutioGUIController.java
@@ -3,10 +3,14 @@ package fxrubkinkuutio;
 import javafx.stage.Stage;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.TextField;
 import fi.jyu.mit.fxgui.*;
+import javafx.application.Application;
 
 /**
  * 
@@ -14,7 +18,7 @@ import fi.jyu.mit.fxgui.*;
  * @version 18.1.2024
  *
  */
-public class RubikinkuutioGUIController {
+public class RubikinkuutioGUIController extends Application {
     @FXML private TextField Ratkaisu;
     
     @FXML private TextField Aika;
@@ -27,9 +31,40 @@ public class RubikinkuutioGUIController {
         Platform.exit();
     }
     
+    @FXML private void handleTallenna() {
+        Dialogs.showMessageDialog("Ei vielä toimi");
+    }
+    
+    @FXML private void handleLisaa() {
+        ModalController.showModal(RubikinkuutioGUIController.class.getResource("RubikinkuutioLisaaView.fxml"), null,null,null);
+    }
 
-    private static void launch(String[] args) {
-        // TODO Auto-generated method stub
-        
+    @FXML private void handlePoista() {
+        Dialogs.showMessageDialog("Ei vielä toimi");
+    }
+    
+    @FXML private void handleApua() {
+        Dialogs.showMessageDialog("Ei vielä toimi");
+    }
+    
+    @FXML private void handleMuokkaa() {
+        Dialogs.showMessageDialog("Ei vielä toimi");
+    }
+    
+    @FXML private void handleTulosta() {
+        Dialogs.showMessageDialog("Ei vielä toimi");
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("RubikinkuutioLisaaView.fxml"));
+            Scene scene = new Scene(root,350,500);
+            scene.getStylesheets().add(getClass().getResource("rubikinkuutio.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
